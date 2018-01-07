017aef29a3bd54843f2d4b21d723c174bdb46b71
DeflatedPickle <DeflatedPickle@gmail.com>
2018-01-07 16:55:41 +0000

    Added an init file

---
+++ swine/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""""""
+
+__title__ = "swine"
+
+__author__ = "DeflatedPickle/Dibbo"
+__copyright__ = "Copyright (c) 2018 Dibbo"
+__credits__ = ["DeflatedPickle/Dibbo"]
+
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "DeflatedPickle/Dibbo"
+__email__ = "DeflatedPickle@gmail.com"
+__status__ = "Development"
