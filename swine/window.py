a1fb1ef5db476c05e3e24bcc9bd64c64e9013ce2
DeflatedPickle <DeflatedPickle@gmail.com>
2018-01-07 20:10:44 +0000

    Updated window.py

--- swine/window.py
+++ swine/window.py
@@ -5,21 +5,48 @@
 import pyglet
 
 class Window(pyglet.window.Window):
-    def __init__(self):
-        pyglet.window.Window.__init__(self)
+    def __init__(self, style=pyglet.window.Window.WINDOW_STYLE_DEFAULT, vsync=False):
+        # type: (str) -> None
+        pyglet.window.Window.__init__(self, style=style, vsync=vsync)
 
+        self._icon = None
         self._fullscreen = False
         self._min_size = (0, 0)
         self._max_size = (0, 0)
 
     def title(self, title):
         # type: (str) -> str
+        """
+        Set and return the title of the window.
+
+        :param title:
+        :return:
+        """
         self.set_caption(title)
 
         return self.get_caption()
 
+    def icon(self, image):
+        # type: (pyglet.image.AbstractImage) -> str
+        """
+        Sets and returns the icon of the window.
+        
+        :param image:
+        :return:
+        """
+        self._icon = image
+        self.set_icon(image)
+
+        return self._icon
+
     def fullscreen(self, fullscreen):
         # type: (bool) -> bool
+        """
+        Set and return if the window is fullscreen.
+
+        :param fullscreen:
+        :return:
+        """
         self._fullscreen = fullscreen
         self.set_fullscreen(fullscreen)
 
@@ -27,6 +54,13 @@ class Window(pyglet.window.Window):
 
     def min_size(self, width, height):
         # type: (int, int) -> tuple[int, int]
+        """
+        Set and return the minimum size of the window.
+
+        :param width:
+        :param height:
+        :return:
+        """
         self._min_size = (width, height)
         self.set_minimum_size(width, height)
 
@@ -34,6 +68,13 @@ class Window(pyglet.window.Window):
 
     def max_size(self, width, height):
         # type: (int, int) -> tuple[int, int]
+        """
+        Set and return the maximum size of the window.
+
+        :param width:
+        :param height:
+        :return:
+        """
         self._max_size = (width, height)
         self.set_maximum_size(width, height)
 
@@ -41,18 +82,29 @@ class Window(pyglet.window.Window):
 
     def size(self, width, height):
         # type: (int, int) -> tuple[int, int]
+        """
+        Set and return the size of the window.
+
+        :param width:
+        :param height:
+        :return:
+        """
         self.set_size(width, height)
 
         return self.get_size()
 
     def location(self, x, y):
         # type: (int, int) -> tuple[int, int]
+        """
+        Set and return the location of the window.
+
+        :param x:
+        :param y:
+        :return:
+        """
         self.set_location(x, y)
 
         return self.get_location()
-
-    def geometry(self):
-        pass
 
     def on_draw(self):
         self.clear()
