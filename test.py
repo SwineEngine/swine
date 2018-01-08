abf096545820566e0fb0e52639ff86d12198d2e5
Dibbo <DeflatedPickle@gmail.com>
2018-01-08 11:52:27 +0000

    Updated test

--- test.py
+++ test.py
@@ -4,15 +4,13 @@ import pyglet
 window = swine.Window()
 window.title("Test")
 
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+label = swine.gui.Label(window, text="Hello World!", x=window.width // 2, y=window.height // 2)
+
 
 @window.event
 def on_draw():
     window.clear()
     label.draw()
+
 
 window.mainloop()
