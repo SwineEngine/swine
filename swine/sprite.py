6d1f1b1ba6b6ef72a4b99e72fe3aaa18b51fcda3
Dibbo <DeflatedPickle@gmail.com>
2018-01-09 19:26:40 +0000

    Updated the sprite class

--- swine/sprite.py
+++ swine/sprite.py
@@ -8,15 +8,15 @@ from swine import GameObject
 from swine import Scene
 
 
-class Sprite(GameObject):
+class Sprite(GameObject, pyglet.sprite.Sprite):
     def __init__(self, scene, image, scale=1):
         # type: (Scene, pyglet.image.AbstractImage) -> None
         GameObject.__init__(self, scene)
+        pyglet.sprite.Sprite.__init__(self, img=image, batch=scene.batch)
         self._scene = scene
         self.image = image
         self.scale = scale
 
-        self.sprite = pyglet.sprite.Sprite(img=self.image, batch=scene.batch)
-        self.sprite.scale = self.scale
+        self.scale = self.scale
 
-        self._scene.object_list.append(self.sprite)
+        self._scene.object_list.append(self)
