a6d4361927c6502e6511b69662003fadacfa4ca1
Dibbo <DeflatedPickle@gmail.com>
2018-01-10 11:55:43 +0000

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""""""
+
+import json
+from setuptools import setup
+
+with open("swine/info.json") as info:
+    info_json = json.loads(info)
+
+    setup(name=info_json["name"],
+          version=info_json["version"],
+          description=info_json["description"],
+          author=info_json["author"],
+          author_email=info_json["author_email"],
+          url=info_json["url"],
+          license=info_json["license"],
+          classifiers=info_json["classifiers"],
+          keywords=info_json["keywords"],
+          packages=info_json["packages"])
