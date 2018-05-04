882c793b4059f006c2a9a6e27ea788ed46776827
DeflatedPickle <DeflatedPickle@gmail.com>
2018-05-04 08:07:04 +0100

    Added a rectangle class

---
+++ swine/shape/rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""""""
+
+import pyglet
+
+from swine import GameObject, Scene
+from .polygon import Polygon
+
+
+class Rectangle(Polygon):
+    def __init__(self, scene, width, height, fill=True, x=0, y=0, layer=0, colours=[]):
+        # type: (Scene, int, int, bool, int, int, int, list[str]) -> None
+        GameObject.__init__(self, scene=scene)
+        points = [x, y, x + width, y,
+                  x + width, y + height, x, y + height]
+        Polygon.__init__(self, scene, fill, 4, x, y, layer, points, colours=colours)
--- test.py
+++ test.py
@@ -87,5 +87,6 @@ class Pig(swine.Sprite):
 pig = Pig()
 
 line = swine.Line(scene_one, 100, 5, 100, 50, 0, swine.RED)
+# rect = swine.Rectangle(scene_one, 100, 50, True, 50, 50, 0, swine.GREEN)
 
 window.mainloop()
