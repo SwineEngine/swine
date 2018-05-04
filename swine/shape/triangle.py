8e9ff0e9e7c12027f0df3279a879871f269efb89
DeflatedPickle <DeflatedPickle@gmail.com>
2018-05-04 09:40:53 +0100

    Added a parameter for the tip placement

--- swine/shape/triangle.py
+++ swine/shape/triangle.py
@@ -9,9 +9,17 @@ from .polygon import Polygon
 
 
 class Triangle(Polygon):
-    def __init__(self, scene, width, height, fill=True, x=0, y=0, layer=0, colours=[]):
-        # type: (Scene, int, int, bool, int, int, int, list[str]) -> None
+    def __init__(self, scene, width, height, tip_placement="center", fill=True, x=0, y=0, layer=0, colours=[]):
+        # type: (Scene, int, int, str, bool, int, int, int, list[str]) -> None
         GameObject.__init__(self, scene=scene)
+
+        if tip_placement == "center":
+            tip = (width // 2)
+        elif tip_placement == "left":
+            tip = 0
+        elif tip_placement == "right":
+            tip = width
+
         points = [x, y, x + width, y,
-                  x + (width // 2), y + height]
+                  x + tip, y + height]
         Polygon.__init__(self, scene, fill, 3, x, y, layer, points, colours=colours)
