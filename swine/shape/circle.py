53b70f62f6ff7c5595193b9f92b269d08d10a179
DeflatedPickle <DeflatedPickle@gmail.com>
2018-05-04 11:51:11 +0100

    Converted circle to an ellipse

--- swine/shape/circle.py
+++ swine/shape/circle.py
@@ -2,27 +2,12 @@
 # -*- coding: utf-8 -*-
 """"""
 
-import math
-
-import pyglet
-
 from swine import GameObject, Scene
-from .polygon import Polygon
+from .ellipse import Ellipse
 
 
-class Circle(Polygon):
+class Circle(Ellipse):
     def __init__(self, scene, size, segments=3, fill=True, x=0, y=0, layer=0, colours=[]):
         # type: (Scene, int, int, bool, int, int, int, list[str]) -> None
         GameObject.__init__(self, scene=scene)
-        points = []
-
-        for i in range(segments):
-            angle = math.radians(float(i) / segments * 360)
-
-            _x = size * math.cos(angle) + x
-            _y = size * math.sin(angle) + y
-
-            points.append(int(_x))
-            points.append(int(_y))
-
-        Polygon.__init__(self, scene, fill, segments, x, y, layer, points, colours=colours)
+        Ellipse.__init__(self, scene, size, size, segments, fill, x, y, layer, colours=colours)
