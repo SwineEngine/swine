e87845e3d9672ad9ee5338a57cf6f906cf29c9d6
DeflatedPickle <DeflatedPickle@gmail.com>
2018-05-05 02:57:31 +0100

    Moved colour handling to a method

--- swine/shape/polygon.py
+++ swine/shape/polygon.py
@@ -13,16 +13,10 @@ class Polygon(GameObject):
         GameObject.__init__(self, scene=scene)
         self._layer = layer
         self._scene = scene
+        self._point_total = point_total
         self._x = x
         self._y = y
-
-        if isinstance(colours, list):
-            while len(colours) < point_total:
-                colours.append(colours[-1])
-
-            colours = tuple(sum(colours, ()))
-        elif isinstance(colours, tuple):
-            colours *= point_total
+        self._colours = self._colour_handler(colours)
 
         if fill:
             mode = pyglet.gl.GL_POLYGON
@@ -30,9 +24,21 @@ class Polygon(GameObject):
             mode = pyglet.gl.GL_LINE_LOOP
 
         self.batch = pyglet.graphics.Batch()
-        self.batch.add(point_total, mode, None,
-                       ('v2i', points),
-                       ('c3B', colours))
+        self._shape = self.batch.add(point_total, mode, None,
+                                     ('v2i', points),
+                                     ('c3B', self._colours))
         self._scene.batch_list.append(self.batch)
 
         self._scene.object_list.append(self)
+
+    def _colour_handler(self, colours):
+        if isinstance(colours, list):
+            while len(colours) < self._point_total:
+                colours.append(colours[-1])
+
+            colours = tuple(sum(colours, ()))
+        elif isinstance(colours, tuple):
+            colours *= self._point_total
+
+        return colours
+
