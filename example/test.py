c856576a19889090d5e20647af9b82f2da6f3be1
DeflatedPickle <DeflatedPickle@gmail.com>
2018-09-03 03:06:40 +0100

    Updated example

--- example/test.py
+++ example/test.py
@@ -12,11 +12,17 @@ import swine.component.physics.collider
 import swine.graphics
 from swine.component import SpriteRenderer, Transform
 from swine.component.physics import RigidBody
+from swine.component.sound import AudioSource
 from swine.input.xbox_controller import XBoxController
+from swine.sound import Mixer, Group
 
 window = swine.window.Window(False)
 
 scene_one = swine.window.Scene(window, pymunk.Vec2d(0, 0), 0.1)
+
+music = Group(75)
+effects = Group(50)
+mixer = Mixer(scene_one, [music, effects])
 
 
 class FollowPlayer(swine.object.Component):
@@ -29,7 +35,8 @@ class FollowPlayer(swine.object.Component):
         transform.position = Vec2d(player_rigid.body.position.x, player_rigid.body.position.y)
 
 
-camera = swine.object.GameObject(scene_one, "Camera", [swine.component.Transform(), swine.component.Viewport(), FollowPlayer()])
+camera = swine.object.GameObject(scene_one, "Camera",
+                                 [swine.component.Transform(), swine.component.Viewport(), FollowPlayer()])
 
 
 class PlayerMove(swine.object.Component):
@@ -49,6 +56,10 @@ class PlayerMove(swine.object.Component):
 
     def update(self, dt):
         sprite = self.parent.get_component(SpriteRenderer)
+        sound = self.parent.get_component(AudioSource)
+
+        if self.input.get_key(key.F):
+            sound.play()
 
         force = pymunk.Vec2d(0, 0)
 
@@ -96,20 +107,21 @@ class PlayerMove(swine.object.Component):
 
 pig_sprite = swine.graphics.Sprite("pig_idle_0.png", swine.object.Anchor.MIDDLE_CENTER)
 pig = swine.object.GameObject(scene_one, "Player", [PlayerMove(),
-                                          swine.component.Transform(),
-                                          swine.component.SpriteRenderer(pig_sprite, 6),
-                                          swine.component.physics.RigidBody(1, False, False),
-                                          swine.component.physics.collider.BoxCollider()])
+                                                    swine.component.Transform(),
+                                                    swine.component.SpriteRenderer(pig_sprite, 6),
+                                                    swine.component.physics.RigidBody(1, False, False),
+                                                    swine.component.physics.collider.BoxCollider(),
+                                                    swine.component.sound.AudioSource(mixer, "untitled.wav", effects)])
 
 box_sprite = swine.graphics.Sprite("metal_box.png", swine.object.Anchor.MIDDLE_CENTER)
 box = swine.object.GameObject(scene_one, "Box1", [swine.component.Transform(Vec2d(150, 0)),
-                                          swine.component.SpriteRenderer(box_sprite, 4),
-                                          swine.component.physics.RigidBody(),
-                                          swine.component.physics.collider.BoxCollider()])
+                                                  swine.component.SpriteRenderer(box_sprite, 4),
+                                                  swine.component.physics.RigidBody(),
+                                                  swine.component.physics.collider.BoxCollider()])
 
 box2 = swine.object.GameObject(scene_one, "Box2", [swine.component.Transform(Vec2d(-150, 0)),
-                                           swine.component.SpriteRenderer(box_sprite, 4),
-                                           swine.component.physics.RigidBody(),
-                                           swine.component.physics.collider.BoxCollider()])
+                                                   swine.component.SpriteRenderer(box_sprite, 4),
+                                                   swine.component.physics.RigidBody(),
+                                                   swine.component.physics.collider.BoxCollider()])
 
 window.mainloop()
